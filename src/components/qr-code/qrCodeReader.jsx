bb70d52c17b61562347f546c0633c2de68733994
p.kaka <pauljoseph5000@gmail.com>
2024-07-16 10:29:40 +0100

    add qr-scan

--- src/components/qr-code/qrCodeReader.jsx
+++ src/components/qr-code/qrCodeReader.jsx
@@ -1,8 +1,36 @@
 import { useState } from "react";
-import QrReader from "react-qr-scanner";
+// import QrReader from "react-qr-scanner";
+
+// function QrCodeReader() {
+//   const [scannedData, setScannedData] = useState("");
+
+//   const handleScan = (data) => {
+//     if (data) {
+//       setScannedData(data);
+//     }
+//   };
+
+//   const handleError = (err) => {
+//     console.error(err);
+//   };
+
+//   return (
+//     <div className="">
+//       <QrReader
+//         delay={100} // Delay between scans in milliseconds (optional)
+//         style={{ height: 240, width: 320 }} // Customize size
+//         onError={handleError} // Handle errors (optional)
+//         onScan={handleScan}
+//         facingMode="rear"
+//       />
+//       <p>Scanned Data: {scannedData}</p>
+//     </div>
+//   );
+// }
+import QrScanner from "@yudiel/react-qr-scanner";
 
 function QrCodeReader() {
-  const [scannedData, setScannedData] = useState("");
+  const [scannedData, setScannedData] = useState(null);
 
   const handleScan = (data) => {
     if (data) {
@@ -10,22 +38,15 @@ function QrCodeReader() {
     }
   };
 
-  const handleError = (err) => {
-    console.error(err);
-  };
-
   return (
     <div className="">
-      <QrReader
-        delay={100} // Delay between scans in milliseconds (optional)
-        style={{ height: 240, width: 320 }} // Customize size
-        onError={handleError} // Handle errors (optional)
+      <QrScanner
         onScan={handleScan}
-        facingMode="rear"
+        delay={1000} // Delay between scans in milliseconds (optional)
+        constraints={{ video: { facingMode: "environment" } }} // Choose environment camera (back-facing)
       />
-      <p>Scanned Data: {scannedData}</p>
+      <div className="">scannedData:{scannedData}</div>
     </div>
   );
 }
-
 export default QrCodeReader;
